c00a10c13851c2aa6bc1e84471d74bd0d8007560
camdentadhg <camdent@gmail.com>
2024-02-27 10:09:17 -0600

    search functionality

--- app.py
+++ app.py
@@ -7,12 +7,12 @@ from flask_debugtoolbar import DebugToolbarExtension
 
 app = Flask(__name__)
 
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///cupcakes'
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-# app.config['SQLALCHEMY_ECHO'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///cupcakes_test'
-app.config['SQLALCHEMY_ECHO'] = False
-app.config['TESTING'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///cupcakes'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///cupcakes_test'
+# app.config['SQLALCHEMY_ECHO'] = False
+# app.config['TESTING'] = True
 app.config['SECRET_KEY'] = "secret"
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
@@ -73,12 +73,10 @@ def delete_cupcake(id):
 
 
 
-# 9 add tests to ensure 404 is returned when cupcakes don't exist (get/patch/delete)
-# 8 add functionality to search for cupcakes (type in term, see newly filtered list of cupcakes)
 # 7 refactor javascript code to be object oriented using class methods (fetchAllCupcakes and createCupcakes and instance methods for updating/deleting/searching)
 # 6 refactor HTML page to use WTForms (create forms.py too)
 # 5 enhance search functionality so it searches as you type
-# 4 add functionality on front end to update a cupcake
+# 4 add functionality on front end to view, update, and delete a cupcake
 # 3 add new model/table for ingredients
 # 2 allow adding ingredients to each cupcake when adding or editing cupcakes
 # 1 create page to add/edit ingredients
--- static/script.js
+++ static/script.js
@@ -5,24 +5,71 @@ const $cupcakeFlavorInput = $('.cupcake-flavor');
 const $cupcakeSizeInput = $('.cupcake-size');
 const $cupcakeRatingInput = $('.cupcake-rating');
 const $cupcakeImageInput = $('.cupcake-image');
+const $searchCupcakeForm = $('#search-cupcake-form');
 let cupcakeData = {};
+let cupcakes = {};
 
 $(document).ready(getCupcakes);
 
 async function getCupcakes() {
     try{
         console.log('starting getCupcakes')
-        const cupcakes = await axios.get('/api/cupcakes');
-        for (cupcake of cupcakes.data.cupcakes){
-            console.log('starting for statement');
-            const $cupcakeLi = $(`<ul><img src=${cupcake.image} class="img-thumbnail border-0" style="max-width:125px"> <b>${cupcake.flavor}</b> (${cupcake.size}, ${cupcake.rating} stars)</ul>`);
-            $cupcakeLi.addClass('fs-3');
-            $cupcakeList.append($cupcakeLi);
-        }
+        cupcakes = await axios.get('/api/cupcakes');
+        displayCupcakes(cupcakes.data.cupcakes);
     } catch(error) {
         alert("No cupcakes found")
+    }};
+
+function displayCupcakes(array){
+    console.log('entering display cupcakes')
+    console.log(array);
+    $cupcakeList.empty()
+    for (cupcake of array){
+        console.log('starting for statement');
+        const $cupcakeLi = $(`<ul><img src=${cupcake.image} class="img-thumbnail border-0" style="max-width:125px"> <b>${cupcake.flavor}</b> (${cupcake.size}, ${cupcake.rating} stars)</ul>`);
+        $cupcakeLi.addClass('fs-3');
+        $cupcakeList.append($cupcakeLi);
+}}
+
+$searchCupcakeForm.submit(function(event){
+    event.preventDefault();
+    console.log('search form submitted');
+    searchCupcakes();
+})
+
+function searchCupcakes(){
+    const searchTerm = $('.cupcake-search').val();
+    const numericSearchTerm = parseInt(searchTerm);
+    const cupcakeResults = [];
+    for (cupcake of cupcakes.data.cupcakes){
+        console.log('entering for cupcake of cupcakes');
+        console.log(cupcake);
+        for (key in cupcake){
+            console.log('entering for key in cupcake');
+            console.log(key);
+            if (key === 'id' || key === 'rating'){
+                console.log('entering numeric search');
+                if (cupcake[key] === numericSearchTerm){
+                    if (cupcakeResults.includes(cupcake) === false){
+                        cupcakeResults.push(cupcake);
+                        console.log('CUPCAKE ADDED');
+                    }
+                }
+            }
+            else if (isNaN(numericSearchTerm)){
+                console.log('entering string search');
+                if (cupcake[key].search(searchTerm) != -1){
+                    if (cupcakeResults.includes(cupcake) === false){
+                        cupcakeResults.push(cupcake);
+                        console.log('CUPCAKE ADDED');
+                    }
+                }
+            }
+        }
     }
-};
+    console.log(cupcakeResults);
+    displayCupcakes(cupcakeResults);
+}
 
 $cupcakeForm.submit(function(event){
     event.preventDefault();
